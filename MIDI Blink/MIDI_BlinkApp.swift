e09bedb658af8ac9ab50be68f86aae7c45affbf3
Pete Yandell <pete@notahat.com>
2023-06-15 15:16:35 +1000

    Initial Commit

---
+++ MIDI Blink/MIDI_BlinkApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIDI_BlinkApp.swift
+//  MIDI Blink
+//
+//  Created by Peter Yandell on 15/6/2023.
+//
+
+import SwiftUI
+
+@main
+struct MIDI_BlinkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

27e235f288c4d74cdc3e4514ce75603f55e95dcf
Pete Yandell <pete@notahat.com>
2023-06-15 18:57:36 +1000

    Make a bunch of MIDISourceManager private

--- MIDI Blink/MIDISourceManager.swift
+++ MIDI Blink/MIDISourceManager.swift
@@ -2,10 +2,10 @@ import CoreMIDI
 
 @MainActor
 class MIDISourceManager: ObservableObject {
-    var clientRef: MIDIClientRef = 0
-    var portRef: MIDIPortRef = 0
-    
     @Published var sources: [MIDISource] = []
+
+    private var clientRef: MIDIClientRef = 0
+    private var portRef: MIDIPortRef = 0
     
     init() {
         createMIDIClient()
@@ -13,7 +13,7 @@ class MIDISourceManager: ObservableObject {
         updateSources()
     }
     
-    func createMIDIClient() {
+    private func createMIDIClient() {
         let status = MIDIClientCreateWithBlock("MIDI Blink" as CFString, &clientRef) { _ in
             Task.detached { await self.updateSources() }
         }
@@ -23,8 +23,8 @@ class MIDISourceManager: ObservableObject {
         }
     }
     
-    func createMIDIPort() {
-        let status = MIDIInputPortCreateWithProtocol(clientRef, "MIDI Blink" as CFString, ._1_0, &portRef) { _, refCon in
+    private func createMIDIPort() {
+        let status = MIDIInputPortCreateWithProtocol(clientRef, "MIDI Blink" as CFString, ._2_0, &portRef) { _, refCon in
             let endpointRef = refCon!.load(as: MIDIEndpointRef.self)
             Task.detached { await self.processReceivedMessages(endpointRef) }
         }
@@ -34,7 +34,7 @@ class MIDISourceManager: ObservableObject {
         }
     }
     
-    func updateSources() {
+    private func updateSources() {
         let numberOfSources = MIDIGetNumberOfSources()
         sources = (0 ..< numberOfSources).map { index in
             let endpointRef = MIDIGetSource(index)
@@ -46,7 +46,7 @@ class MIDISourceManager: ObservableObject {
         }
     }
     
-    func listenForMessagesFrom(_ endpointRef: MIDIEndpointRef) {
+    private func listenForMessagesFrom(_ endpointRef: MIDIEndpointRef) {
         // TODO: Fix this memory leak!
         // Just to get things working, I'm deliberately allocating memory that's never deallocated.
         // This is obviously not ok, so I need to come up with a better approach.
@@ -60,7 +60,7 @@ class MIDISourceManager: ObservableObject {
         }
     }
     
-    func processReceivedMessages(_ endpointRef: MIDIEndpointRef) {
+    private func processReceivedMessages(_ endpointRef: MIDIEndpointRef) {
         if let index = sources.firstIndex(where: { $0.endpointRef == endpointRef }) {
             sources[index].hasReceivedMessages = true
         }

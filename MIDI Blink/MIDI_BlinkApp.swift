5682af4c88894b6845b53ae8a0de2a3dbcf74bb0
Pete Yandell <pete@notahat.com>
2023-06-26 13:59:04 +1000

    Make the help menu open the support page.

--- MIDI Blink/MIDI_BlinkApp.swift
+++ MIDI Blink/MIDI_BlinkApp.swift
@@ -2,6 +2,8 @@ import SwiftUI
 
 @main
 struct MIDI_BlinkApp: App {
+    @Environment(\.openURL) var openURL
+
     @StateObject private var sourceManager = MIDISourceManager()
 
     var body: some Scene {
@@ -10,6 +12,17 @@ struct MIDI_BlinkApp: App {
                 sources: sourceManager.sources,
                 showError: sourceManager.errorOccurred
             )
+        }
+        .commands {
+            CommandGroup(replacing: .help) {
+                Button("MIDI Blink Help") { openHelp() }
+            }
+        }
+    }
+    
+    private func openHelp() {
+        if let url = URL(string: "https://notahat.com/midi-blink/support/") {
+            openURL(url)
         }
     }
 }
